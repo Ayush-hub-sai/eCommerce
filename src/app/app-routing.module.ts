5713ff25d950194d0f9da09ec604356cccd7a899
Ayush-hub-sai <jibana0987@gmail.com>
2023-11-19 06:29:15 +0530

    eCommerce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SalesComponent } from './pages/sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'sale', component: SalesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
